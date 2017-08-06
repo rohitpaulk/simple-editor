b4a1782353e8a1df035a21180f2be943ee2fa75a
Paul Kuruvilla <rohitpaulk@gmail.com>
2017-08-06 18:44:54 +0530

    Added entry point

--- project.clj
+++ project.clj
@@ -1,6 +1,7 @@
 (defproject simple-editor "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
+  :description "A simple text editor"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]])
+  :dependencies [[org.clojure/clojure "1.8.0"]]
+  :main simple-editor.core)
