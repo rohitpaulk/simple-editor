1453d14f3606c1aefccc6ce2d10458c1f57c7df6
Paul Kuruvilla <rohitpaulk@gmail.com>
2017-08-06 21:14:25 +0530

    Add render func

--- src/simple_editor/core.clj
+++ src/simple_editor/core.clj
@@ -1,10 +1,26 @@
 (ns simple-editor.core)
 
+(defn render
+  "Renders the buffer + cursor"
+  [buffer, _]
+  (def lines (:lines buffer))
+  (dorun (map println lines)))
+
+(defn process-input
+  "Processes one input character from the terminal"
+  [char]
+  (println "Processing input"))
+
 (defn open-editor
   "Opens the editor with the given file"
   [file-path]
   (println (format "Here are the contents from (%s)\n" file-path))
-  (println (slurp file-path)))
+  (def file-contents (slurp file-path))
+  (def lines (clojure.string/split-lines file-contents))
+  (def buffer {:lines lines})
+  (while true (do 
+                (render buffer nil) 
+                (process-input))
 
 (defn -main
   "Entry point"
