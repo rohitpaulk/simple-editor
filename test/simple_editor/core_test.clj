47c2b107c247bd4938f23d75d0ad87653580bbe5
Paul Kuruvilla <rohitpaulk@gmail.com>
2017-08-08 20:35:25 +0530

    Fix def-in-def declarations in tests

--- test/simple_editor/core_test.clj
+++ test/simple_editor/core_test.clj
@@ -4,66 +4,57 @@
 
 (deftest test-simple-keys
   (testing "characters"
-    (def before-state {:lines ["123" "456"] :cursor {:x 0 :y 0}})
-    (def actual (process-key \n before-state))
-    (def expected {:lines ["n123" "456"] :cursor {:x 1 :y 0}})
-
-    (is (= actual expected)))
+    (let [before-state {:lines ["123" "456"] :cursor {:x 0 :y 0}}
+          actual (process-key \n before-state)
+          expected {:lines ["n123" "456"] :cursor {:x 1 :y 0}}]
+      (is (= actual expected))))
 
   (testing "up"
-    (def before-state {:lines ["a" "b"] :cursor {:x 0 :y 1}})
-    (def actual (process-key :up before-state))
-    (def expected {:lines ["a" "b"] :cursor {:x 0 :y 0}})
-
-    (is (= actual expected)))
+    (let [before-state {:lines ["a" "b"] :cursor {:x 0 :y 1}}
+          actual (process-key :up before-state)
+          expected {:lines ["a" "b"] :cursor {:x 0 :y 0}}]
+      (is (= actual expected))))
 
   (testing "down"
-    (def before-state {:lines ["a" "b"] :cursor {:x 0 :y 0}})
-    (def actual (process-key :down before-state))
-    (def expected {:lines ["a" "b"] :cursor {:x 0 :y 1}})
-
-    (is (= actual expected))))
+    (let [before-state {:lines ["a" "b"] :cursor {:x 0 :y 0}}
+          actual (process-key :down before-state)
+          expected {:lines ["a" "b"] :cursor {:x 0 :y 1}}]
+      (is (= actual expected)))))
 
 (deftest test-enter
   (testing "creates new line if content of line is empty"
-    (def before-state {:lines [""] :cursor {:x 0 :y 0}})
-    (def actual (process-key :enter before-state))
-    (def expected {:lines ["" ""] :cursor {:x 0 :y 1}})
-
-    (is (= actual expected)))
+    (let [before-state {:lines [""] :cursor {:x 0 :y 0}}
+          actual (process-key :enter before-state)
+          expected {:lines ["" ""] :cursor {:x 0 :y 1}}]
+      (is (= actual expected))))
 
   (testing "creates new line if at end of line"
-    (def before-state {:lines ["abc"] :cursor {:x 3 :y 0}})
-    (def actual (process-key :enter before-state))
-    (def expected {:lines ["abc" ""] :cursor {:x 0 :y 1}})
-
-    (is (= actual expected)))
+    (let [before-state {:lines ["abc"] :cursor {:x 3 :y 0}}
+          actual (process-key :enter before-state)
+          expected {:lines ["abc" ""] :cursor {:x 0 :y 1}}]
+      (is (= actual expected))))
 
   (testing "splits contents if in middle of line"
-    (def before-state {:lines ["abc"] :cursor {:x 1 :y 0}})
-    (def actual (process-key :enter before-state))
-    (def expected {:lines ["a" "bc"] :cursor {:x 0 :y 1}})
-
-    (is (= actual expected))))
+    (let [before-state {:lines ["abc"] :cursor {:x 1 :y 0}}
+          actual (process-key :enter before-state)
+          expected {:lines ["a" "bc"] :cursor {:x 0 :y 1}}]
+      (is (= actual expected)))))
 
 (deftest test-backspace
   (testing "does nothing if at start of document"
-    (def before-state {:lines ["abc"] :cursor {:x 0 :y 0}})
-    (def actual (process-key :backspace before-state))
-    (def expected {:lines ["abc"] :cursor {:x 0 :y 0}})
-
-    (is (= actual expected)))
+    (let [before-state {:lines ["abc"] :cursor {:x 0 :y 0}}
+          actual (process-key :backspace before-state)
+          expected {:lines ["abc"] :cursor {:x 0 :y 0}}]
+      (is (= actual expected))))
 
   (testing "deletes character if in middle of line"
-    (def before-state {:lines ["abc"] :cursor {:x 1 :y 0}})
-    (def actual (process-key :backspace before-state))
-    (def expected {:lines ["bc"] :cursor {:x 0 :y 0}})
-
-    (is (= actual expected)))
+    (let [before-state {:lines ["abc"] :cursor {:x 1 :y 0}}
+          actual (process-key :backspace before-state)
+          expected {:lines ["bc"] :cursor {:x 0 :y 0}}]
+      (is (= actual expected))))
 
   (testing "merges with previous line if at start of next"
-    (def before-state {:lines ["abc" "def"] :cursor {:x 0 :y 1}})
-    (def actual (process-key :backspace before-state))
-    (def expected {:lines ["abcdef"] :cursor {:x 3 :y 0}})
-
-    (is (= actual expected))))
+    (let [before-state {:lines ["abc" "def"] :cursor {:x 0 :y 1}}
+          actual (process-key :backspace before-state)
+          expected {:lines ["abcdef"] :cursor {:x 3 :y 0}}]
+      (is (= actual expected)))))
